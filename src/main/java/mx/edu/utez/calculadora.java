9e612b9fa7bd6b154097403b6ef21a175b18819c
Guillermo Lara <20253ds044@utez.edu.mx>
2026-05-20 13:50:25 -0600

    [NEW] clase calculadora y constructor

---
+++ src/main/java/mx/edu/utez/calculadora.java
@@ -0,0 +1,12 @@
+package mx.edu.utez;
+
+public class calculadora {
+
+    private double numero1;
+    private double numero2;
+
+    public calculadora(double numero1, double numero2) {
+        this.numero1 = numero1;
+        this.numero2 = numero2;
+    }
+}
